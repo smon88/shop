4bcd4134a1ce6e9c5d87cec82dd35a95f557e58f
smon88 <santimonsa88@gmail.com>
2025-12-21 21:49:52 -0500

    png to webp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { FooterComponent } from "./shared/components/footer/footer.component";
-import { CookieConsentComponent } from "./shared/cookie-consent/cookie-consent.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CookieConsentComponent } from './shared/cookie-consent/cookie-consent.component';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from "./shared/components/loading/loading.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, CookieConsentComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    CookieConsentComponent,
+    CommonModule,
+    LoadingComponent
+],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  loading = true;
+
+  ngAfterViewInit(): void {
+    requestAnimationFrame(() => {
+      this.loading = false;
+    });
+  }
+}
